72dcc2fd06b64ddc2a720fc5ffdd681159f4c785
brnguy <nguyen.bryan15@gmail.com>
2022-03-26 20:56:43 -0700

    Working Fetch Signle Event

--- src/components/pages/EventDetails.jsx
+++ src/components/pages/EventDetails.jsx
@@ -1,5 +1,26 @@
-export default function EventDetails() {
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom'
+import axios from "axios"
+
+export default function EventDetails({ event }) {
+    const { id } = useParams()
+    const [details, setDetails]= useState([])
+
+    useEffect(() => { 
+        async function fetchData(){
+            const eventDetails = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/events/${id}`)
+            setDetails(eventDetails.data)
+    }
+    fetchData()
+  }, [])
+
     return(
-        <h1>Profile page</h1>
+        <>
+            <div>
+                <h1>{details.title}</h1>
+                <h2>Host: {details.host}</h2>
+            </div>
+        </>
     )
+
 }
