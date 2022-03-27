55495321384c128e3d5ed36545f0a0d1745838fa
tristonpalacios <triston.j.palacios@gmail.com>
2022-03-27 11:14:50 -0700

    utilized set state to implement non-crashing host display in event details

--- src/components/pages/EventDetails.jsx
+++ src/components/pages/EventDetails.jsx
@@ -7,11 +7,16 @@ export default function EventDetails({ event }) {
     const { id } = useParams()
     const [details, setDetails]= useState([])
     const [attendeeCount, setAttendeeCount] = useState()
+    const [host,setHost]= useState()
 
     useEffect(() => { 
         async function fetchData(){
             const eventDetails = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/events/${id}`)
             setDetails(eventDetails.data)
+            setHost(eventDetails.data.host.name)
+
+            
+            
     }
     fetchData()
   }, [])
@@ -22,7 +27,7 @@ export default function EventDetails({ event }) {
         <>
             <div className="left" id="details">
                 <h1>{details.title}</h1>
-                <h2>Host: {details.host}</h2>
+                <h2>Host: {host} </h2>
                 <h3>{details.category}</h3>
                 <p>{details.date}</p>
                 <p>{details.address}</p>
