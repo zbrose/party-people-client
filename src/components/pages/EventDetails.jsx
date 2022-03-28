1d9479b297b3025a6866e9f5fa3597d591b24452
brnguy <nguyen.bryan15@gmail.com>
2022-03-28 15:05:36 -0700

    Fixed Bug

--- src/components/pages/EventDetails.jsx
+++ src/components/pages/EventDetails.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 import axios from "axios"
 import { Tab, Tabs } from 'react-bootstrap';
-import Button from '../Button';
 const dayjs = require('dayjs')
 
 export default function EventDetails({ currentUser }) {
