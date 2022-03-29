e34178a29043671a1aef838992b985e7a66b2b42
zbrose <zrbrose@gmail.com>
2022-03-29 11:34:22 -0700

    carousel changes to home page

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -1,11 +1,9 @@
-
 import Carousel from 'react-bootstrap/Carousel'
-
 import Events from '../Events'
 
 
 
-export default function Welcome({events, currentUser, filter, setFilter, setEvents}) {
+export default function Welcome({events, currentUser, filter, setFilter}) {
  
   return (
     <>
@@ -20,31 +18,28 @@ export default function Welcome({events, currentUser, filter, setFilter, setEven
           />
           <Carousel.Caption>
             <h3>Welcome to The Party People App</h3>
-            <p>Party</p>
           </Carousel.Caption>
         </Carousel.Item>
 
         <Carousel.Item interval={5000}>
           <img
             className="d-block w-100"
-            src="http://placekitten.com/400/150"
+            src="images/gaming.jpg"
             alt="Second slide"
           />
           <Carousel.Caption>
             <h3>Where People Who Party...</h3>
-            <p>Party Hard</p>
           </Carousel.Caption>
         </Carousel.Item>
 
         <Carousel.Item interval={5000}>
           <img
             className="d-block w-100"
-            src="http://placekitten.com/400/150"
+            src="images/playingCards.jpg"
             alt="Third slide"
           />
           <Carousel.Caption>
             <h3>...Find Parties To Party At.</h3>
-            <p>No, Harder</p>
           </Carousel.Caption>
         </Carousel.Item>
         
@@ -52,7 +47,7 @@ export default function Welcome({events, currentUser, filter, setFilter, setEven
       
       <br />
 
-      <Events events={events} filter={filter} setFilter={setFilter} currentUser={currentUser} setEvents={setEvents} />
+      <Events events={events} filter={filter} setFilter={setFilter} currentUser={currentUser}  />
 
     </>
     
