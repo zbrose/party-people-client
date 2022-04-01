506fef0959453ca3692658788da5533a03a9752c
tristonpalacios <triston.j.palacios@gmail.com>
2022-03-31 20:04:59 -0700

    commit to pull

--- src/components/pages/HypeMeter.jsx
+++ src/components/pages/HypeMeter.jsx
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import lowHype from "../low.png";
-import semiHype from "../semihype.png";
-import ultraHype from "../ultrahype.png";
-import middleHype from "../middleHype.png";
-import highHype from "../highHype.png";
+
 
 
 export default function HypeMeter({ details }) {
@@ -44,33 +40,33 @@ export default function HypeMeter({ details }) {
 
   const displayHype = () => {
     if (numCount < 25) {
-      meter = <img src={lowHype} alt="" style={{ width: 250, height: 250 }} />;
+      meter = <img src="images/low.png" alt="" style={{ width: 250, height: 250 }} />;
     } else if (numCount < 50) {
       meter = (
         <h1>
           {" "}
-          <img src={semiHype} alt="" style={{ width: 250, height: 250 }} />
+          <img src="images/low.png" alt="" style={{ width: 250, height: 250 }} />
         </h1>
       );
     } else if (numCount < 75) {
       meter = (
         <h1>
           {" "}
-          <img src={middleHype} alt="" style={{ width: 250, height: 250 }} />
+          <img src="images/low.png"alt="" style={{ width: 250, height: 250 }} />
         </h1>
       );
     } else if (numCount < 100) {
       meter = (
         <h1>
           {" "}
-          <img src={highHype} alt="" style={{ width: 250, height: 250 }} />
+          <img src="images/low.png" alt="" style={{ width: 250, height: 250 }} />
         </h1>
       );
     } else if (numCount > 99) {
       meter = (
         <h1>
           {" "}
-          <img src={ultraHype} alt="" style={{ width: 250, height: 250 }} />
+          <img src="images/low.png" alt="" style={{ width: 250, height: 250 }} />
         </h1>
       );
     }
